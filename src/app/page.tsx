f6493009e803908d7db3467bf6267ef95777e525
Pedro Mantovani <pedromantovani26@gmail.com>
2024-05-25 10:38:41 -0300

    fix redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default  async function Home() {
   const session =  await auth()
 
     if (!session) {
-        redirect('/login')
+        redirect('/auth/login')
     }
   return (
     <h1>Bem Vindo de Volta, {session.user?.name}!</h1>
