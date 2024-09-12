92c1c8212eec41e3ff1079c63f503a273fa8ac23
Pedro Mantovani <pedromantovani26@gmail.com>
2024-09-12 15:35:16 -0300

    fix : unnecessary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import Image from "next/image";
-import { auth, signOut } from "../../auth";
-import { redirect } from "next/navigation";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import CreatePost from "@/components/CreatePost";
-import ShowPosts from "@/components/ShowPosts";
 import Side from "@/components/side";
 
 export default  async function Home() {
-  const session =  await auth()
-
   return (
     <>
-      {/* <div className="mx-auto max-w-2xl space-y-6 py-12 md:py-16"> */}
         <Side/>
-      {/* <ShowPosts/> */}
-      {/* <CreatePost/> */}
-      {/* </div> */}
     </>
   );
 }
